0cbf501486f1c44e4b39a080f1b52ff9be479bed
Akbar Gherbal <ghurbal.akbar@gmail.com>
2022-12-01 23:05:31 +0300

    cdn and babel modification

--- react_cdn_01/app.js
+++ react_cdn_01/app.js
@@ -1,8 +1,18 @@
-const App = () => {
-    return (<div>hello world</div>)
- }
+// const App = () => {
+//     return (<div>hello world</div>)
+//  }
  
- ReactDOM.render(<App />, document.getElementById('root'))
+//  ReactDOM.render(<App />, document.getElementById('root'))
 
 
- console.log(`Hello world! from external file`)
+//  console.log(`Hello world! from external file`)
+
+
+
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
+  }
+  
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  const element = <Welcome name="Sara" />;
+  root.render(element);
